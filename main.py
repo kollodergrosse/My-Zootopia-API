723ceba1da64f797f425c31b1f1df5da1e5d97c3
Alexander Kolle <alexanderkolle@av23-009.mo>
2026-06-11 09:48:18 +0200

    implemented api call for animal data

--- main.py
+++ main.py
@@ -31,11 +31,11 @@ def serialize_animal(animals_data):
     for animal in animals_data:
         try:
             output += '<li class="cards__item">'
-            output += f'<div class="card__title">{animal.get("name")}</div>'
+            output += f'<div class="card__title">{animal["name"]}</div>'
             output += '<p class="card__text">'
-            output += f'<strong>Diet:</strong> {animal.get("characteristics", {}).get("diet")}<br/>\n'
-            output += f'<strong>Location:</strong> {animal.get("locations")[0]}<br/>\n'
-            output += f'<strong>Type:</strong> {animal.get("characteristics").get("type")}<br/>\n'
+            output += f'<strong>Diet:</strong> {animal["characteristics"]["diet"]}<br/>\n'
+            output += f'<strong>Location:</strong> {animal["locations"][0]}<br/>\n'
+            output += f'<strong>Type:</strong> {animal["characteristics"]["type"]}<br/>\n'
             output += "</p>\n"
             output += "</li>\n"
 
